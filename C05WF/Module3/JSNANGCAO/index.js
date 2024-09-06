dbcd570909afd621d6f16dfe78415331ac2038e5
NamBe <hoangnam3182003@gmail.com>
2024-09-06 18:50:56 +0700

    Signed-off-by: NamBe <hoangnam3182003@gmail.com>

---
+++ C05WF/Module3/JSNANGCAO/index.js
@@ -0,0 +1,23 @@
+// let a = 1;
+// let b = 2;
+// let c = b;
+// c = 10;
+// console.log('b', b);
+// console.log('c', c);
+
+// let obj = { // Object Literal (Cú pháp khởi tạo nhanh 1 đối tượng ở trong Js)
+//     name: "linh",
+//     age: 26,
+//     gender: "Nam"
+// }
+
+// let arr = [1, 4, 63, 4];
+// -> kiểu tham chiếu (Object, Array, Function);
+
+// let obj = {
+//     name: "linh",
+//     age: 26
+// }
+
+// let obj2 = obj;
+// obj2.name = "MÈO"
---
+++ C05WF/Module3/JSNANGCAO/index2.js
@@ -0,0 +1,6 @@
+let arr = [1, 5, 3, 4, 32];
+arr.forEach(
+    (item)  => {
+        console.log(item);
+    }
+);
